e472839bd1a63b5f209b669034ee00a801de51f4
Willems Davy <joyrider3774@hotmail.com>
2023-07-31 01:33:17 +0200

    Use Default Font for Credits

--- src/gamestates/credits.c
+++ src/gamestates/credits.c
@@ -19,7 +19,7 @@ void CreditsDeInit()
 
 void Credits()
 {
-	char *Tekst = "Puzzle Land is programmed by\nWillems Davy. Graphics are made\nusing Paint Shop Pro 7. A Big thanks\ngoes out to Yann R. Fernandez for\ncreating some of the graphics.\nPuzzle Land is a remake of\nDaedalian opus for gameboy by\nVic Tokai Inc. 1990\n\nWillems Soft 2006";
+	char *Tekst = "Puzzle Land is programmed by\nWillems Davy. Graphics are made\nusing gimp. A Big thanks goes out\nto Yann R. Fernandez for creating\nsome of the graphics.\n\nPuzzle Land is a remake of\nDaedalian opus for gameboy by\nVic Tokai Inc. 1990";
 
 	if(GameState == GSCreditsInit)
 	{
@@ -35,15 +35,12 @@ void Credits()
 		CreditsNrOfChars++;
 
 	pd->graphics->drawBitmap(Background, 0, 0, kBitmapUnflipped);
+	pd->graphics->pushContext(NULL);
+	pd->graphics->setFont(NULL);
 	pd->graphics->drawText("CREDITS", strlen("CREDITS"), kASCIIEncoding, WINDOW_WIDTH / 2 - 32, 20);
 	pd->graphics->drawText(Tekst, CreditsNrOfChars,kASCIIEncoding, 30, 40);
-	//SDL_BlitSurface(Background,NULL,Screen,NULL);
-	//TTF_SetFontStyle(font,TTF_STYLE_UNDERLINE);
-	//SDL_Color Color1 = {0,0,0,0};
-	/*WriteText(Screen,font,"CREDITS",7,Screen-> w / 2 - 32,20,0,Color1);
-	TTF_SetFontStyle(font,TTF_STYLE_NORMAL);*/
-	//WriteText(Screen,font,Tekst,CreditsNrOfChars,30,40,0,Color1);
-	
+	pd->graphics->popContext();
+
 	if (GameState != GSCredits)
 		CreditsDeInit();
 }
