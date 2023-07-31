5e3ff847eaf4f4c7f79cfc0dab63f9d2b6ef7d56
Willems Davy <joyrider3774@hotmail.com>
2023-07-31 03:15:23 +0200

    simpler cloud drawing, since we clip screen rect

--- src/gameobjects/ccloud.c
+++ src/gameobjects/ccloud.c
@@ -26,49 +26,7 @@ CCloud* CCloud_Create(const int XIn,const int YIn,float XiIn,CloudStyles Style)
 
 void CCloud_Draw(CCloud* Cloud)
 {
-	//SDL_Rect SrcRect,DstRect;
-	if ((Cloud->X < 0) && (Cloud->X >= -Cloud->Width))
-	{
-		/*SrcRect.x = abs((int)Cloud->X);
-		SrcRect.y = 0;
-		SrcRect.w = abs((int)Cloud->X) + Cloud->Width;
-		SrcRect.h = Cloud->Height;
-		DstRect.x = (int)Cloud->X;
-		DstRect.y = Cloud->Y;
-		DstRect.w = (int)Cloud->X + Cloud->Width;
-		DstRect.h = Cloud->Height;
-		SDL_BlitSurface(Cloud->Image,&SrcRect,Screen,&DstRect);
-		*/
-		DrawBitmapSrcRec(Cloud->Image, (int)Cloud->X, Cloud->Y, abs((int)Cloud->X), 0, abs((int)Cloud->X) + Cloud->Width, Cloud->Height, kBitmapUnflipped);
-	}
-	else
-		if((Cloud->X >=0) && (Cloud->X <= WINDOW_WIDTH - 1 - Cloud->Width))
-		{
-			/*SrcRect.x = 0;
-			SrcRect.y = 0;
-			SrcRect.w = Cloud->Width;
-			SrcRect.h = Cloud->Height;
-			DstRect.x = (int)Cloud->X;
-			DstRect.y = Cloud->Y;
-			DstRect.w = Cloud->Width;
-			DstRect.h = Cloud->Height;
-			SDL_BlitSurface(Cloud->Image,&SrcRect,Screen,&DstRect);*/
-			DrawBitmapSrcRec(Cloud->Image, (int)Cloud->X, Cloud->Y, 0, 0, Cloud->Width, Cloud->Height, kBitmapUnflipped);
-		}
-		else
-			if((Cloud->X > WINDOW_WIDTH - 1 - Cloud->Width) && (Cloud->X < WINDOW_WIDTH))
-			{
-				/*SrcRect.x = 0;
-				SrcRect.y =0;
-				SrcRect.w = WINDOW_WIDTH -1 - (int)Cloud->X;
-				SrcRect.h = Cloud->Height;
-				DstRect.x = (int)Cloud->X;
-				DstRect.y = Cloud->Y;
-				DstRect.w = WINDOW_WIDTH - 1 -(int)Cloud->X;
-				DstRect.h = Cloud->Height;
-				SDL_BlitSurface(Cloud->Image,&SrcRect,Screen,&DstRect);*/
-				DrawBitmapSrcRec(Cloud->Image, (int)Cloud->X, Cloud->Y, 0, 0, WINDOW_WIDTH - 1 - (int)Cloud->X, Cloud->Height, kBitmapUnflipped);
-			}
+	pd->graphics->drawBitmap(Cloud->Image, (int)Cloud->X, Cloud->Y, kBitmapUnflipped);
 }
 
 void CCloud_Move(CCloud* Cloud)
